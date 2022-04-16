1588aea39b00bdf5c3a52449aa9e4f3a15c44cb5
nonJerry <jeremy.mank@web.de>
2022-04-16 23:44:10 +0200

    Fix wrong method call

--- database.py
+++ database.py
@@ -381,10 +381,10 @@ class Database(metaclass=Singleton):
             settings.update_one({"name": "supported_idols"}, {'$push': {'supported_idols': {"name": name.lower(), "guild_id": guild_id}}})
 
     def remove_vtuber(self, guild_id: int):
-        self._get_general_settings().update({'name': 'supported_idols'}, {'$pull': { 'supported_idols': {'guild_id': guild_id}}})
+        self._get_general_settings().update_one({'name': 'supported_idols'}, {'$pull': { 'supported_idols': {'guild_id': guild_id}}})
 
     def remove_multi_talent_vtuber(self, guild_id: int, name: str):
-        self._get_general_settings().update({'name': 'supported_idols'}, {'$pull': { 'supported_idols': {'name': name, 'guild_id': guild_id}}})
+        self._get_general_settings().update_one({'name': 'supported_idols'}, {'$pull': { 'supported_idols': {'name': name, 'guild_id': guild_id}}})
 
     def add_multi_server(self, guild_id: int):
         self._get_general_settings().update_one({"name": "multi_server"}, {'$push': {'ids': guild_id}})
