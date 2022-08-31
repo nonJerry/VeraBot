fb7a75636e884f866c288994ee472dbb69772cd4
Jeremy <jeremy.mank@students.hs-mainz.de>
2022-08-31 06:08:56 +0200

    Do not sync on startup

--- app.py
+++ app.py
@@ -308,7 +308,6 @@ coroutines = (
 # Main Coroutine
 async def background_main():
     await add_cogs()
-    await bot.tree.sync()
     # Add Log Channel processing view
     bot.add_view(PersistentView(member_handler))
     await bot.wait_until_ready()
